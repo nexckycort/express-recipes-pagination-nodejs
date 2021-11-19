4708710d41462bcda56be0e591ca247ad695976c
ncortina <ncortina@cidenet.com.co>
2021-11-19 14:26:46 -0500

    feat: add solution

--- app.js
+++ app.js
@@ -18,5 +18,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/recipes', recipeRouter);
 
 module.exports = app;
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,4 +1,23 @@
 var recipes = require('../recipes.json');
 var router = require('express').Router();
 
+router.get('/step/:id', (req, res) => {
+  const { id } = req.params;
+  const { elapsedTime = 0 } = req.query;
+
+  const foundId = recipes.find((recipe) => recipe.id === +id);
+  if (!foundId) return res.status(400).send('NOT_FOUND');
+
+  let index = 0;
+  for (let i = 0; i < foundId.timers.length; i++) {
+    const time = foundId.timers[i];
+    if (time >= +elapsedTime) {
+      index = i;
+      break;
+    }
+  }
+
+  res.status(200).json({ index });
+});
+
 module.exports = router;
